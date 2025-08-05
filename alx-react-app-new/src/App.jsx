39d3f5ed397d422561065307f84aac1ee9795dd9
Currentsparrow <crrntsparrow@gmail.com>
2025-08-05 23:17:54 +0000

    added basic strucure

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,25 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
-import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import Counter from './components/Counter'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY
-
-export async function fetchUser(username) {
-  const response = await fetch(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: `token ${GITHUB_API_KEY}`,
-    },
-  })
-  return response.json()
-}
 
 function Home() {
   return <h2>Welcome to GitHub User Search!</h2>
